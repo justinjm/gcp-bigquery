aaa212f2ab6c969dc9639f0788aa5a0168a9d8a7
Justin Marciszewski <justinjm@google.com>
2023-01-24 13:38:47 -0500

    fixing final query

--- src/02_walkthrough.sh
+++ src/02_walkthrough.sh
@@ -30,14 +30,12 @@ bq query --location=US --destination_table $dataset_id.table_id_partitioned --no
 echo "[ INFO ] List all BigQuery tables within $dataset_id"
 bq ls $dataset_id
 
-
 echo "[ INFO ] Deleting BigQuery dataset names tempdataset1"
 bq rm -rf tempdataset1
-
 
 echo "[ INFO ] Displaying Schema"
 bq show "$project_id:$dataset_id.$table_id"
 
 
 echo "[ INFO ] Run BigQuery query"
-bq query "SELECT count(*) as count FROM $project_id:$dataset_id.$table_id"
+bq query "SELECT count(*) as count FROM `$project_id.$dataset_id.$table_id`"
